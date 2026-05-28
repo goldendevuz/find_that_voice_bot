164787e3100f2c7e1eef09db9917ebb6c3aa4e91
goldendevuz <goldendevuz@gmail.com>
2026-05-28 14:25:03 +0500

    Switch IDs to UUIDs via BaseModel and add pagination (list_per_page=10) in admin

--- voices/admin.py
+++ voices/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from .models import BotUser, Voice
 
+
+class BaseAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+    class Meta:
+        abstract = True
+
 @admin.register(BotUser)
-class BotUserAdmin(admin.ModelAdmin):
+class BotUserAdmin(BaseAdmin):
     list_display = ('telegram_id', 'username', 'first_name', 'joined_at')
     search_fields = ('telegram_id', 'username', 'first_name')
 
 @admin.register(Voice)
-class VoiceAdmin(admin.ModelAdmin):
+class VoiceAdmin(BaseAdmin):
     list_display = ('id', 'owner', 'description', 'usage_count', 'created_at')
     search_fields = ('description', 'owner__username', 'owner__telegram_id')
     list_filter = ('created_at',)
